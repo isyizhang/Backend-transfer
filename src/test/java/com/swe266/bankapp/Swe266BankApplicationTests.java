c825576d5da566873320c380b4e3ec0cb0954272
isyizhang <08280828zy@gmail.com>
2023-05-15 21:18:53 -0700

    Initial Commit

---
+++ src/test/java/com/swe266/bankapp/Swe266BankApplicationTests.java
@@ -0,0 +1,13 @@
+package com.swe266.bankapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Swe266BankApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
