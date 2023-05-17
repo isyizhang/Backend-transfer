ea2401169b043373bec91feb98f30e256dfab6ac
wendycy <yche7@uci.edu>
2023-05-16 22:16:01 -0700

    Add method for repository

--- src/main/java/com/swe266/bankapp/repository/UserRepository.java
+++ src/main/java/com/swe266/bankapp/repository/UserRepository.java
@@ -7,6 +7,8 @@ import org.springframework.stereotype.Repository;
 
 import java.math.BigDecimal;
 
+import static org.springframework.util.Assert.notNull;
+
 @Repository
 public class UserRepository {
 
@@ -62,5 +64,94 @@ public class UserRepository {
         return count != null && count > 0;
     }
 
+
+    /**
+     * To add deposit to existing account.
+     * @param username
+     * @param amount
+     */
+    public void deposit(String username, double amount) {
+        notNull(username);
+        notNull(amount);
+
+        if (existsUserByUsername(username)) {
+            String depositSql = "UPDATE User SET balance = balance + ? WHERE username = ?";
+            jdbcTemplate.update(depositSql, amount, username);
+        } else {
+            throw new IllegalArgumentException("User with username '" + username + "' does not exist.");
+        }
+    }
+
+    /**
+     * To check account balance for existing account.
+     * @param username
+     * @return
+     */
+    public double checkBalance(String username) {
+        notNull(username);
+
+        String sql = "SELECT balance FROM User WHERE username = ?";
+
+        try {
+            Double balance = jdbcTemplate.queryForObject(sql, Double.class, username);
+            System.out.println("Balance for user '" + username + "': " + balance);
+            return balance;
+        } catch (EmptyResultDataAccessException e) {
+            throw new IllegalArgumentException("User with username '" + username + "' does not exist.");
+        }
+    }
+
+
+    /**
+     * To withdraw money from existing account.
+     * @param username
+     * @param amount
+     */
+    public void withdraw(String username, double amount) {
+        notNull(username);
+        notNull(amount);
+
+        if (existsUserByUsername(username)) {
+            double currentBalance = checkBalance(username);
+            double newBalance = currentBalance - amount;
+
+            if (newBalance >= 0) {
+                String withdrawSql = "UPDATE User SET balance = ? WHERE username = ?";
+                jdbcTemplate.update(withdrawSql, newBalance, username);
+                System.out.println("Withdrawal of " + amount + " from user '" + username + "' successful.");
+            } else {
+                throw new IllegalArgumentException("Insufficient balance for user '" + username + "'.");
+            }
+        } else {
+            throw new IllegalArgumentException("User with username '" + username + "' does not exist.");
+        }
+    }
+
+
+    /**
+     * To support user login.
+     * @param username
+     * @param password
+     * @return
+     */
+    public User logIn(String username, String password) {
+        notNull(username);
+        notNull(password);
+
+        String sql = "SELECT * FROM User WHERE username = ? AND password = ?";
+
+        try {
+            return jdbcTemplate.queryForObject(sql, (rs, rowNum) -> {
+                User user = new User();
+                user.setId(rs.getLong("id"));
+                user.setUsername(rs.getString("username"));
+                user.setPassword(rs.getString("password"));
+                user.setBalance(rs.getDouble("balance"));
+                return user;
+            }, username, password);
+        } catch (EmptyResultDataAccessException e) {
+            throw new IllegalArgumentException("Invalid username or password.");
+        }
+    }
 
 }
