fa4ed8ceba478fc42a35928a1ce00ffbcc1b7df0
wendycy <yche7@uci.edu>
2023-05-17 22:54:53 -0700

    Update sql file

--- bankAcct.sql
+++ bankAcct.sql
@@ -2,8 +2,8 @@ CREATE DATABASE bank;
 USE bank;
 
 CREATE TABLE user (
-                      id INT AUTO_INCREMENT PRIMARY KEY,
-                      username VARCHAR(50) NOT NULL,
-                      password VARCHAR(100) NOT NULL,
-                      balance DOUBLE NOT NULL DEFAULT 0
+    id INT AUTO_INCREMENT PRIMARY KEY,
+    username VARCHAR(127) NOT NULL,
+    password VARCHAR(127) NOT NULL,
+    balance DOUBLE NOT NULL DEFAULT 0
 );
